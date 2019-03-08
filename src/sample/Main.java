df9394826789cc92f06f44bb084bdbcb389cebf1
martinhug <mhug@hsr.ch>
2019-03-08 16:14:44 +0100

    changes tutorial 5 (Open new Window)

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,41 +6,33 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Stage;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
+
 
 import java.awt.*;
 
 public class Main extends Application {
 
   Stage window;
-  Scene scene1, scene2;
+  Button button;
 
 
   @Override
   public void start(Stage primaryStage) throws Exception {
     window = primaryStage;
-
-    Label label = new Label("Welcome to the First Scene");
-    Button button1 = new Button("Go To Scnene 2");
-    button1.setOnAction(e -> window.setScene(scene2));
-
-    VBox layout1 = new VBox(20);
-    layout1.getChildren().addAll(label, button1);
-    scene1 = new Scene(layout1, 200, 200);
-
-    Button button2 = new Button("Go To Scnene 1");
-    button2.setOnAction(e -> window.setScene(scene1));
-
-    StackPane layout2 = new StackPane();
-    layout2.getChildren().add(button2);
-    scene2 = new Scene(layout2, 600, 300);
-
-    window.setScene(scene1);
     window.setTitle("Windowtitle");
+
+    button = new Button("Click Me!");
+    button.setOnAction(e -> AlertBox.display("WindowTitle", "Hey handsome"));
+
+    StackPane layout = new StackPane();
+    layout.getChildren().add(button);
+    Scene scene = new Scene(layout, 300, 250);
+    window.setScene(scene);
     window.show();
   }
 
