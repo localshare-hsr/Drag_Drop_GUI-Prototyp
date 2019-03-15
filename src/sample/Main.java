6e0c7a3da4b7a1e0571b0da107672e2dc534229e
martinhug <mhug@hsr.ch>
2019-03-15 16:27:09 +0100

    changes tutorial 15 (ListView)
    Listing where you can select multiple items

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,18 +1,17 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.ComboBox;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
   private Stage window;
-  private ComboBox<String> coBox;
+  private ListView<String> listview;
 
   @Override
   public void start(Stage primaryStage) throws Exception {
@@ -20,30 +19,32 @@ public class Main extends Application {
     window.setTitle("checkbox");
 
     Button button = new Button("Click Me");
+    listview = new ListView<>();
+    listview.getItems().addAll("Iron Man", "Titanic", "Contact");
+    listview.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
 
-    coBox = new ComboBox<String>();
+    button.setOnAction(e -> buttonClicked());
 
-    coBox.setEditable(true);
-
-    // get Items
-    coBox.getItems().addAll("Die Hard", "Star Wars", "Batman");
-
-    coBox.setPromptText("What is your favorite movie?");
-    button.setOnAction(e -> printMovie());
-
-    coBox.setOnAction(e -> System.out.println("User Selected: " + coBox.getValue()));
     // Layout
     VBox layout1 = new VBox(10);
     layout1.setPadding(new Insets(20, 20, 20, 20));
-    layout1.getChildren().addAll(coBox, button);
+    layout1.getChildren().addAll(listview, button);
 
     Scene scene = new Scene(layout1, 300, 200);
     window.setScene(scene);
     window.show();
   }
 
-  private void printMovie(){
-    System.out.println(coBox.getValue());
+  private void buttonClicked() {
+    String message = "";
+    ObservableList<String> movies;
+    movies = listview.getSelectionModel().getSelectedItems();
+
+    for (String m : movies) {
+      message += m + "\n";
+    }
+
+    System.out.println(message);
   }
 
   public static void main(String[] args) {
