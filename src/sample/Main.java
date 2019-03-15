aabaf16e8c2c483c95c5f18526ff6f90fd8a0aca
martinhug <mhug@hsr.ch>
2019-03-15 10:19:44 +0100

    changes tutorial 9 (Grid View)
    GridPane (Simple Login Screen

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,19 +1,13 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.stage.Stage;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
-
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
 import java.awt.*;
 
@@ -28,25 +22,38 @@ public class Main extends Application {
     window = primaryStage;
     window.setTitle("Windowtitle");
 
-    HBox topMenu = new HBox();
-    Button buttonA = new Button("File");
-    Button buttonB = new Button("Edit");
-    Button buttonC = new Button("New");
+    GridPane grid = new GridPane();
+    grid.setPadding(new Insets(10,10,10,10)); //Insets = Padding from the edge
+    // cells itself don't have padding
+    grid.setVgap(8); // vertical gap between cells
+    grid.setHgap(10); //horizontal gap between cells
 
-    topMenu.getChildren().addAll(buttonA,  buttonB, buttonC);
+    // Name Label
+    Label nameLabel = new Label("Username");
+    GridPane.setConstraints(nameLabel, 0,0);
 
-    VBox leftMenu = new VBox();
-    Button buttonD = new Button("D");
-    Button buttonE = new Button("E");
-    Button buttonF = new Button("F");
-    leftMenu.getChildren().addAll(buttonD, buttonE, buttonF);
+    //Name Input
+    TextField nameInput = new TextField("Default Text");
+    GridPane.setConstraints(nameInput, 1, 0);
 
-    BorderPane borderPane = new BorderPane();
-    borderPane.setTop(topMenu);
-    borderPane.setLeft(leftMenu);
+    // Password Label
+    Label passLabel = new Label("Password");
+    GridPane.setConstraints(passLabel, 0,1);
 
+    //Password Input
+    //Name Input
+    TextField passInput = new TextField();
+    passInput.setPromptText("Password");
+    GridPane.setConstraints(passInput, 1, 1);
 
-    Scene scene = new Scene(borderPane, 300, 250);
+    Button loginbutton = new Button("Login");
+    loginbutton.setOnAction(e -> System.out.println("Login Processing..."));
+    GridPane.setConstraints(loginbutton, 1,2);
+
+    grid.getChildren().addAll(nameLabel,nameInput, passInput, passLabel, loginbutton);
+
+    Scene scene = new Scene(grid, 300, 200);
+
     window.setScene(scene);
     window.show();
   }
