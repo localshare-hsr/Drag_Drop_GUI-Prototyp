592640d25733959432600af0a78008e2ecb94df5
martinhug <mhug@hsr.ch>
2019-03-18 16:13:08 +0100

    changes tutorial 19(TableView)
    Add a Row with Buttons and Text

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -3,20 +3,22 @@ package sample;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TreeItem;
-import javafx.scene.control.TreeView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import javax.xml.soap.Text;
 
 public class Main extends Application {
 
   private Stage window;
   private TableView<Product> table;
+  private TextField nameInput, priceInput, quantityInput;
 
   @Override
   public void start(Stage primaryStage) throws Exception {
@@ -38,14 +40,39 @@ public class Main extends Application {
     quantitycolumn.setMinWidth(200);
     quantitycolumn.setCellValueFactory(new PropertyValueFactory<>("Quantity"));
 
+
+    //name input
+    nameInput = new TextField();
+    nameInput.setPromptText("Name");
+    nameInput.setMinWidth(100);
+
+    //price input
+    priceInput = new TextField();
+    priceInput.setPromptText("Price");
+    priceInput.setMinWidth(100);
+
+    //quantity input
+    quantityInput = new TextField();
+    quantityInput.setPromptText("Quantity");
+    quantityInput.setMinWidth(100);
+
+
     table = new TableView<>();
     table.setItems(getProcuct());
     table.getColumns().addAll(namecolumn, pricecolumn, quantitycolumn);
 
 
+    //Button
+    Button add = new Button("Add");
+    Button delete = new Button("Delete");
+
+    HBox hbox = new HBox();
+    hbox.setPadding(new Insets(10,10,10,10));
+    hbox.setSpacing(10);
+    hbox.getChildren().addAll(nameInput, priceInput, quantityInput, add, delete);
 
     VBox vBox = new VBox();
-    vBox.getChildren().addAll(table);
+    vBox.getChildren().addAll(table, hbox);
 
     StackPane layout = new StackPane();
 
@@ -54,7 +81,7 @@ public class Main extends Application {
     window.show();
   }
 
-  public ObservableList<Product> getProcuct() {
+  private ObservableList<Product> getProcuct() {
     ObservableList<Product> products = FXCollections.observableArrayList();
     products.add(new Product("Laptop", 850, 5));
     products.add(new Product("Ball", 0.4, 58));
