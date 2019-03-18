ebb93fb377ced4c54f6d79df043e451b654ebf3f
martinhug <mhug@hsr.ch>
2019-03-18 16:24:12 +0100

    changes tutorial 20(TableView)
    Add Functionality to Buttons

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,14 +5,15 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
-import javax.xml.soap.Text;
 
 public class Main extends Application {
 
@@ -40,34 +41,33 @@ public class Main extends Application {
     quantitycolumn.setMinWidth(200);
     quantitycolumn.setCellValueFactory(new PropertyValueFactory<>("Quantity"));
 
-
-    //name input
+    // name input
     nameInput = new TextField();
     nameInput.setPromptText("Name");
     nameInput.setMinWidth(100);
 
-    //price input
+    // price input
     priceInput = new TextField();
     priceInput.setPromptText("Price");
     priceInput.setMinWidth(100);
 
-    //quantity input
+    // quantity input
     quantityInput = new TextField();
     quantityInput.setPromptText("Quantity");
     quantityInput.setMinWidth(100);
-
 
     table = new TableView<>();
     table.setItems(getProcuct());
     table.getColumns().addAll(namecolumn, pricecolumn, quantitycolumn);
 
-
-    //Button
+    // Button
     Button add = new Button("Add");
+    add.setOnAction(e -> addButtonClicked());
     Button delete = new Button("Delete");
+    delete.setOnAction(e -> deleteButtonClicked());
 
     HBox hbox = new HBox();
-    hbox.setPadding(new Insets(10,10,10,10));
+    hbox.setPadding(new Insets(10, 10, 10, 10));
     hbox.setSpacing(10);
     hbox.getChildren().addAll(nameInput, priceInput, quantityInput, add, delete);
 
@@ -88,7 +88,26 @@ public class Main extends Application {
     products.add(new Product("Toilet", 99, 10));
     products.add(new Product("Cap", 50, 8));
     products.add(new Product("Server", 1000, 45));
-  return products;
+    return products;
+  }
+
+  private void addButtonClicked(){
+    Product product = new Product();
+    product.setName(nameInput.getText());
+    product.setPrice(Double.parseDouble(priceInput.getText()));
+    product.setQuantity(Integer.parseInt(quantityInput.getText()));
+    table.getItems().add(product);
+    nameInput.clear();
+    priceInput.clear();
+    quantityInput.clear();
+  }
+
+  private void deleteButtonClicked(){
+    ObservableList<Product> productselected, allproducts;
+    allproducts = table.getItems();
+    productselected = table.getSelectionModel().getSelectedItems();
+
+    productselected.forEach(allproducts::remove);
   }
 
   public static void main(String[] args) {
