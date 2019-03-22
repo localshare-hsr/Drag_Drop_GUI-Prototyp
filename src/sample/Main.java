abced0bf7e6c9bbdf6399fb814e3446be2702b13
martinhug <mhug@hsr.ch>
2019-03-22 11:05:24 +0100

    changes tutorial 26(CSS)
    Background and Fontsize change

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -9,13 +9,10 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
-import java.awt.*;
-
 public class Main extends Application {
 
   private Stage window;
   Button button;
-
 
   @Override
   public void start(Stage primaryStage) throws Exception {
@@ -23,53 +20,47 @@ public class Main extends Application {
     window.setTitle("Windowtitle");
 
     GridPane grid = new GridPane();
-    grid.setPadding(new Insets(10,10,10,10)); //Insets = Padding from the edge
+    grid.setPadding(new Insets(10, 10, 10, 10)); // Insets = Padding from the edge
     // cells itself don't have padding
     grid.setVgap(8); // vertical gap between cells
-    grid.setHgap(10); //horizontal gap between cells
+    grid.setHgap(10); // horizontal gap between cells
 
     // Name Label
     Label nameLabel = new Label("Username");
-    GridPane.setConstraints(nameLabel, 0,0);
+    GridPane.setConstraints(nameLabel, 0, 0);
 
-    //Name Input
+    // Name Input
     TextField nameInput = new TextField("Default Text");
     GridPane.setConstraints(nameInput, 1, 0);
 
     // Password Label
     Label passLabel = new Label("Password");
-    GridPane.setConstraints(passLabel, 0,1);
+    GridPane.setConstraints(passLabel, 0, 1);
 
-    //Password Input
-    //Name Input
+    // Password Input
+    // Name Input
     TextField passInput = new TextField();
     passInput.setPromptText("Password");
     GridPane.setConstraints(passInput, 1, 1);
 
     Button loginbutton = new Button("Login");
-    loginbutton.setOnAction(e -> {
-            if(getUserAgentStylesheet() == STYLESHEET_MODENA){
-              setUserAgentStylesheet(STYLESHEET_CASPIAN);
-            }
-            else{
-              setUserAgentStylesheet(STYLESHEET_MODENA);
-            }
+    loginbutton.setOnAction(
+        e -> {
+          if (getUserAgentStylesheet() == STYLESHEET_MODENA) {
+            setUserAgentStylesheet(STYLESHEET_CASPIAN);
+          } else {
+            setUserAgentStylesheet(STYLESHEET_MODENA);
+          }
+        });
+    GridPane.setConstraints(loginbutton, 1, 2);
 
-
-    });
-    GridPane.setConstraints(loginbutton, 1,2);
-
-
-
-    grid.getChildren().addAll(nameLabel,nameInput, passInput, passLabel, loginbutton);
+    grid.getChildren().addAll(nameLabel, nameInput, passInput, passLabel, loginbutton);
 
     Scene scene = new Scene(grid, 300, 200);
-
+    scene.getStylesheets().add("/Viper.css");
     window.setScene(scene);
     window.show();
   }
-
-
 
   public static void main(String[] args) {
     launch(args);
