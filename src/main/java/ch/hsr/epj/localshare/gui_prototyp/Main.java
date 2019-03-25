362e3d76386728671b02e7bb033c6d47df9c2534
martinhug <mhug@hsr.ch>
2019-03-25 16:55:35 +0100

    Tutorial 30 (Bindings)
    Binding a Label to a Textfield (dynamic filling)

--- src/main/java/ch/hsr/epj/localshare/gui_prototyp/Main.java
+++ src/main/java/ch/hsr/epj/localshare/gui_prototyp/Main.java
@@ -1,16 +1,11 @@
 package ch.hsr.epj.localshare.gui_prototyp;
 
 
-import ch.hsr.epj.localshare.gui_prototyp.Person;
 import javafx.application.Application;
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.SimpleIntegerProperty;
-import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -21,38 +16,24 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         window = primaryStage;
-        window.setTitle("Windowtitle");
+        window.setTitle("Login Info with Binding");
 
-        Person martin = new Person();
+        //Input and Labels
+        TextField userInput = new TextField();
+        userInput.setMaxWidth(200);
 
-        martin.firstNameProperty().addListener((v, oldValue, newValue) -> {
-            System.out.println("Name Changed to: " + newValue);
-            System.out.println("firstNameProperty(): " + martin.firstNameProperty());
-            System.out.println("getFirstname(): " + martin.getFirstName());
-        });
+        Label fistlabel = new Label("Welcome to the site");
+        Label secondlabel = new Label();
 
-        IntegerProperty x = new SimpleIntegerProperty(3);
-        IntegerProperty y = new SimpleIntegerProperty();
+        HBox bottomtext = new HBox(fistlabel, secondlabel);
+        bottomtext.setAlignment(Pos.CENTER);
 
-        y.bind(x.multiply(10));
+        VBox vbox = new VBox(10, userInput, bottomtext);
+        vbox.setAlignment(Pos.CENTER);
 
-        System.out.println("x" + x.getValue() + "y" + y.getValue());
-        GridPane grid = new GridPane();
-        grid.setPadding(new Insets(10, 10, 10, 10)); // Insets = Padding from the edge
-        // cells itself don't have padding
-        grid.setVgap(8); // vertical gap between cells
-        grid.setHgap(10); // horizontal gap between cells
+        secondlabel.textProperty().bind(userInput.textProperty());
 
-        //Change firstName
-        Button signupbutton = new Button("Sign Up");
-
-        GridPane.setConstraints(signupbutton, 1, 3);
-
-        signupbutton.setOnAction(e -> martin.setFirstName("Märti"));
-
-        grid.getChildren().addAll(signupbutton);
-
-        Scene scene = new Scene(grid, 300, 200);
+        Scene scene = new Scene(vbox, 300, 200);
         window.setScene(scene);
         window.show();
     }
